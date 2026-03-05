552090ed2496b95017f786f074f93597688ee8d5
rachelsigao <rachelsigao@gmail.com>
2026-03-05 17:43:56 -0500

    terraform

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.32.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "rachel-remote-tfstate"
+    key          = "vpc-demo"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+# provider "aws" {
+#   # Configuration options
+# }
