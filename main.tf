eb285e7e257419176bb7985906dc55037f19f83c
rachelsigao <rachelsigao@gmail.com>
2026-03-09 17:35:12 -0400

    terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #roboshop-dev-frontend
 resource "aws_security_group" "main" {
-  # name        =   var.sg_name
-  name        =  "${var.project}-${var.environment}-${var.sg_name}"
+  name        =   var.sg_name
+  # name        =  "${var.project}-${var.environment}-${var.sg_name}"
   description = var.sg_description
   vpc_id      = var.vpc_id
 
